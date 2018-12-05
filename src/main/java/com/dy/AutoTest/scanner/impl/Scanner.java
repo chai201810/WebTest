2d26c63d99da39b17fdf240098ce557902890957
chai201810 <44453537+chai201810@users.noreply.github.com>
2018-12-05 13:50:26 +0800

    Scanner

--- src/main/java/com/dy/AutoTest/scanner/impl/Scanner.java
+++ src/main/java/com/dy/AutoTest/scanner/impl/Scanner.java
@@ -1,4 +1,4 @@
-package com.dy.AutoTest.web.util;
+package com.dy.AutoTest.scanner.impl;
 
 import java.io.File;
 import java.io.FileFilter;
@@ -20,14 +20,12 @@ public class Scanner {
 	 */
 	public Set<Class<?>> getClasses(String path, String packageName) throws Exception {
 
-		// 第一个class类的集合
 		Set<Class<?>> classes = new HashSet<>();
 		// 是否循环迭代
 		boolean recursive = true;
 
 		try {
 			URL url = new URL(path);
-			// 得到协议的名称
 			String protocol = url.getProtocol();
 			// 如果是以文件的形式保存在服务器上
 			if ("file".equals(protocol)) {
@@ -78,31 +76,29 @@ public class Scanner {
 	public static void findAndAddClassesInPackageByFile(String packageName, String packagePath, final boolean recursive,
 			Set<Class<?>> classes) {
 
-		// 获取此包的目录 建立一个File
 		File dir = new File(packagePath);
-		// 如果不存在或者 也不是目录就直接返回
+
 		if (!dir.exists() || !dir.isDirectory()) {
-			// log.warn("用户定义包名 " + packageName + " 下没有任何文件");
 			return;
 		}
-		// 如果存在 就获取包下的所有文件 包括目录
+
 		File[] dirfiles = dir.listFiles(new FileFilter() {
-			// 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
+			// 自定义过滤规则：支持递归的目录或者以.class结尾的文件
 			public boolean accept(File file) {
 				return (recursive && file.isDirectory()) || (file.getName().endsWith(".class"));
 			}
 		});
-		// 循环所有文件
+
 		for (File file : dirfiles) {
-			// 如果是目录 则继续扫描
+
 			if (file.isDirectory()) {
 				findAndAddClassesInPackageByFile(packageName + "." + file.getName(), file.getAbsolutePath(), recursive,
 						classes);
 			} else {
-				// 如果是java类文件 去掉后面的.class 只留下类名
+				// 去掉文件后缀名.class，只留下类名
 				String className = file.getName().substring(0, file.getName().length() - 6);
 				try {
-					// 经过回复同学的提醒，这里用forName有一些不好，会触发static方法，没有使用classLoader的load干净
+					// classLoader比forName的load干净，无需调用static方法
 					classes.add(
 							Thread.currentThread().getContextClassLoader().loadClass(packageName + '.' + className));
 
@@ -121,7 +117,7 @@ public class Scanner {
 		if (!dir.exists() || !dir.isDirectory()) {
 			return;
 		}
-		// 过滤获取目录，or class文件
+		// 自定义过滤规则：目录或者以.class结尾的文件
 		File[] dirfiles = dir.listFiles(pathname -> pathname.isDirectory() || pathname.getName().endsWith("class"));
 		if (dirfiles == null || dirfiles.length == 0) {
 			return;
@@ -132,8 +128,8 @@ public class Scanner {
 			if (f.isDirectory()) {
 				findClassesByFile(pkgName + "." + f.getName(), pkgPath + "/" + f.getName(), classes);
 				continue;
-			} 
-			// 获取类名，干掉 ".class" 后缀
+			}
+			// 获取类名
 			className = f.getName();
 			className = className.substring(0, className.length() - 6);
 			// 加载类
@@ -156,14 +152,13 @@ public class Scanner {
 	public static void main(String[] args) throws Exception {
 
 		URL path = new Scanner().getClass().getClassLoader().getResource("");
-        System.out.println(path);
-		
-		String packageName = "com.dy.AutoTest.OnlineCashier.TestCases.accountmanage";
-		String packagePath = packageName.replaceAll("\\.","/");
-		
-		System.out.println(packagePath);
-		
-		new Scanner().getTests(path + "/" + packagePath, packageName);
+		System.out.println(path);
 
+		String packageName = "com.dy.AutoTest";
+		String packagePath = packageName.replaceAll("\\.", "/");
+
+		System.out.println(packagePath);
+
+		new Scanner().getTests(path + "/" + packagePath, packageName);
 	}
 }
