a6e0a9078000260c269cdeef41942f80e2779a9a
chai201810 <44453537+chai201810@users.noreply.github.com>
2018-12-27 19:25:10 +0800

    Update

--- src/main/java/com/dy/AutoTest/listener/TestResultListener.java
+++ src/main/java/com/dy/AutoTest/listener/TestResultListener.java
@@ -10,38 +10,6 @@ import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
 public class TestResultListener extends TestListenerAdapter {
-
-	protected ITestContext testContext = null; // 这里也是新加的
-	
-	@Override
-	public void onStart(ITestContext testContext) { // 这里也是新加的，用于对context进行统一
-		super.onStart(testContext);
-		this.testContext = testContext;
-	}
-
-	@Override
-	public void onTestFailure(ITestResult tr) {
-		super.onTestFailure(tr);
-		System.out.println(tr.getName() + " 测试用例执行失败！");
-	}
-
-	@Override
-	public void onTestSkipped(ITestResult tr) {
-		super.onTestSkipped(tr);
-		System.out.println(tr.getName() + " 测试用例由于某些原因被跳过！");
-	}
-
-	@Override
-	public void onTestSuccess(ITestResult tr) {
-		super.onTestSuccess(tr);
-		System.out.println(tr.getName() + " 测试用例执行成功！");
-	}
-
-	@Override
-	public void onTestStart(ITestResult tr) {
-		super.onTestStart(tr);
-		System.out.println(tr.getName() + " 测试用例开始执行！");
-	}
 	
 	@Override
 	public void onFinish(ITestContext testContext) {
@@ -52,14 +20,14 @@ public class TestResultListener extends TestListenerAdapter {
 		// collect all id's from passed test
 		Set<Integer> passedTestIds = new HashSet<Integer>();
 		for (ITestResult passedTest : testContext.getPassedTests().getAllResults()) {
-			System.out.println("执行成功的用例 = " + passedTest.getName());
+			System.out.println("Success Case = " + passedTest.getName() + "; Cost Time = " + (passedTest.getEndMillis()-passedTest.getStartMillis()));
 			passedTestIds.add(getId(passedTest));
 		}
 
 		// Eliminate the repeat methods
 		Set<Integer> skipTestIds = new HashSet<Integer>();
 		for (ITestResult skipTest : testContext.getSkippedTests().getAllResults()) {
-			System.out.println("被跳过的用例 = " + skipTest.getName());
+			System.out.println("Skipped Case = " + skipTest.getName() + "; Cost Time = " + (skipTest.getEndMillis()-skipTest.getStartMillis()));
 			int skipTestId = getId(skipTest);
 
 			if (skipTestIds.contains(skipTestId) || passedTestIds.contains(skipTestId)) {
@@ -72,7 +40,7 @@ public class TestResultListener extends TestListenerAdapter {
 		// Eliminate the repeat failed methods
 		Set<Integer> failedTestIds = new HashSet<Integer>();
 		for (ITestResult failedTest : testContext.getFailedTests().getAllResults()) {
-			System.out.println("执行失败的用例 = " + failedTest.getName());
+			System.out.println("Failed Case = " + failedTest.getName() + "; Cost Time = " + (failedTest.getEndMillis()-failedTest.getStartMillis()));
 			int failedTestId = getId(failedTest);
 			if (failedTestIds.contains(failedTestId) || passedTestIds.contains(failedTestId)
 					|| skipTestIds.contains(failedTestId)) {
