587ab21c047a60a22d8cf4b2f29b16d59527bc5a
chai201810 <44453537+chai201810@users.noreply.github.com>
2018-12-27 16:59:15 +0800

    Update LOG

--- src/main/java/com/dy/AutoTest/listener/TestResultListener.java
+++ src/main/java/com/dy/AutoTest/listener/TestResultListener.java
@@ -7,12 +7,10 @@ import java.util.Set;
 
 import org.testng.ITestContext;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.TestListenerAdapter;
-import org.testng.log4testng.Logger;
 
 public class TestResultListener extends TestListenerAdapter {
-
-	private static Logger logger = Logger.getLogger(TestResultListener.class);
 
 	@Override
 	public void onFinish(ITestContext testContext) {
@@ -23,14 +21,14 @@ public class TestResultListener extends TestListenerAdapter {
 		// collect all id's from passed test
 		Set<Integer> passedTestIds = new HashSet<Integer>();
 		for (ITestResult passedTest : testContext.getPassedTests().getAllResults()) {
-			logger.info("执行成功的用例 = " + passedTest.getName());
+			Reporter.log("执行成功的用例 = " + passedTest.getName());
 			passedTestIds.add(getId(passedTest));
 		}
 
 		// Eliminate the repeat methods
 		Set<Integer> skipTestIds = new HashSet<Integer>();
 		for (ITestResult skipTest : testContext.getSkippedTests().getAllResults()) {
-			logger.info("被跳过的用例 = " + skipTest.getName());
+			Reporter.log("被跳过的用例 = " + skipTest.getName());
 			int skipTestId = getId(skipTest);
 
 			if (skipTestIds.contains(skipTestId) || passedTestIds.contains(skipTestId)) {
@@ -43,7 +41,7 @@ public class TestResultListener extends TestListenerAdapter {
 		// Eliminate the repeat failed methods
 		Set<Integer> failedTestIds = new HashSet<Integer>();
 		for (ITestResult failedTest : testContext.getFailedTests().getAllResults()) {
-			logger.info("执行失败的用例 = " + failedTest.getName());
+			Reporter.log("执行失败的用例 = " + failedTest.getName());
 			int failedTestId = getId(failedTest);
 			if (failedTestIds.contains(failedTestId) || passedTestIds.contains(failedTestId)
 					|| skipTestIds.contains(failedTestId)) {
